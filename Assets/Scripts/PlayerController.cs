fa7bed6873d282c2f1a6a2d4b68b038b9666fcc8
subway-aint-fresh <97781497+subway-aint-fresh@users.noreply.github.com>
2023-06-08 03:00:00 +1000

    Fixing player controller script
    - smoothing out the forwards movement (it was bouncing around)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,13 +6,15 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 5f;         // Jump force
     public LayerMask groundLayer;        // Layer(s) considered as ground
     public float groundCheckDistance = 0.1f; // Distance to check for ground contact
+    public float slopeFactor = 5f;       // Factor to control rolling down hills
 
     private Rigidbody rb;                // Reference to the player's Rigidbody component
-    private bool isGrounded = false;     // Check if the player is grounded
+    private bool isGrounded = false;     // Flag to check if the player is grounded
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = Vector3.zero;  // Reset the center of mass to avoid undesired rolling effects
     }
 
     private void Update()
@@ -23,6 +25,18 @@ public class PlayerController : MonoBehaviour
         // Handle player movement
         float moveInput = Input.GetAxisRaw("Horizontal");
         Vector3 movement = new Vector3(moveInput, 0f, 0f) * moveSpeed;
+
+        // Apply slope factor based on the ground normal to roll down hills
+        if (isGrounded)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, Vector3.down, out hit, groundCheckDistance, groundLayer))
+            {
+                float slopeAngle = Vector3.Angle(Vector3.up, hit.normal);
+                movement += hit.normal * slopeFactor * slopeAngle;
+            }
+        }
+
         rb.velocity = new Vector3(movement.x, rb.velocity.y, movement.z);
 
         // Handle player jump
