22d558d392d69c179f2ab36da9eb91fed571a6be
subway-aint-fresh <97781497+subway-aint-fresh@users.noreply.github.com>
2023-06-08 05:35:36 +1000

    Added Next Level Script
    - continue menu popup is set to active when the player enters the EndZone

--- Assets/Scripts/NextLevel.cs
+++ Assets/Scripts/NextLevel.cs
@@ -1,18 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class NextLevel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject continueMenuUI;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            // Activate the continue menu UI
+            continueMenuUI.SetActive(true);
+        }
     }
 }
